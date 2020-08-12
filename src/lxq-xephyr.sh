489398b0fb2b0b030d5d1cb3c6e071ed99b9366e
Philip Crockett <contact@philcrockett.com>
2020-08-12 10:33:22 +0200

    Simpler subcommand discovery

--- src/lxq-xephyr.sh
+++ src/lxq-xephyr.sh
@@ -12,40 +12,15 @@ readonly SCRIPT_DIR=$(dirname "$(readlink -f "${0}")")
 # shellcheck source=/dev/null
 . "${SCRIPT_DIR}/_util.sh"
 
-subcommand_regex="/lxq-xephyr-([a-z]+)\\.sh$"
-readarray -t subcommand_scripts < <(find_subcommand_scripts "${subcommand_regex}")
-
-declare -A subcommands
-for script in "${subcommand_scripts[@]}"
-do
-    if [[ $script =~ ${subcommand_regex} ]]; then
-        subcom="${BASH_REMATCH[1]}"
-        subcommands["${subcom}"]="${script}"
-    else
-        panic "$script did not match regex as expected."
-    fi
-done
-
-function print_subcommand_summary() {
-    full_script_path="${1}"
-    if [[ $full_script_path =~ /lxq-xephyr-([a-z]+)\.sh$ ]]; then
-        command_name="${BASH_REMATCH[1]}"
-        summary=$(LXQ_SHORT_SUMMARY=1 "${full_script_path}")
-        printf "  %s%s\n" "${command_name}" "${summary}" >&2
-    else
-        panic "${full_script_path} did not match regex as expected."
-    fi
-}
+declare -A LXQ_SUBCOMMANDS
+lxq_populate_subcommands "/lxq-xephyr-([a-z]+)\\.sh$"
 
 function show_usage() {
     printf "Usage: lxq xephyr [command]\n" >&2
     printf "\n" >&2
     printf "Available commands:\n" >&2
 
-    for s in "${subcommand_scripts[@]}"
-    do
-        print_subcommand_summary "${s}"
-    done
+    print_subcommand_summaries
 
     printf "\n" >&2
     printf "Flags:\n">&2
@@ -60,8 +35,8 @@ function show_usage_and_exit() {
 function parse_commandline() {
 
     if [ "${#}" -gt "0" ]; then
-        if is_set "${subcommands[${1}]+x}"; then
-            LXQ_COMMAND="${subcommands[${1}]}"
+        if is_set "${LXQ_SUBCOMMANDS[${1}]+x}"; then
+            LXQ_COMMAND="${LXQ_SUBCOMMANDS[${1}]}"
             return # Let subcommands parse the rest of the parameters
         fi
     fi
